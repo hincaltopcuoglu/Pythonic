db2f9fbff1e1265a32cb2026eb1e53ba67966453
htopc <hincal.topcuoglu@gmail.com>
2025-05-17 22:57:49 +0300

    new algorithmic functions added

--- algorithms.py
+++ algorithms.py
@@ -164,7 +164,7 @@ def find_prime() -> list[int]:
 
 
 """
-Write a function that takes the average of 10 randomly entered integers in sequence, 
+Q-9 : Write a function that takes the average of 10 randomly entered integers in sequence, 
 then finds the average of the largest and smallest of these numbers, and returns the difference between these two averages.
 """
 
@@ -179,6 +179,96 @@ def find_mean_diff() -> int:
     return diff
 
 #print(find_mean_diff())
+
+############################################################################################
+
+"""
+Q-10 : Write a function that, given a 4-digit integer, finds the difference between the remainder obtained by dividing the digit in the hundreds place by 3 and the digit in the units place of the same number.
+"""
+
+def find_digit_diff() -> int:
+    user_input = int(input("Enter a four digit number:"))
+    hundereds = user_input % 1000
+    val = (hundereds % 3)
+    tens = hundereds % 100
+    units = tens % 10
+
+    return val - units
+
+#print(find_digit_diff())
+
+
+############################################################################################
+
+
+"""
+Q-11 : Write a function that, given a rational number whose integer part has at most 4 digits, checks if the hundreds digit of the integer part is even, and if so, determines whether the cube of that digit is a perfect square.
+"""
+
+def check_rational_square():
+    user_input = input("Enter a rational numeber whose integer part has at most 4 digits:")
+    integer_part = int((float(user_input)))
+    hundreds = (integer_part // 100) % 10
+    if hundreds % 2 == 0:
+        cube = hundreds ** 3
+        root = int(cube ** 0.5)
+
+        if root * root == cube:
+            return f"The cube of the hundreds digit ({hundreds}) of number {user_input} is a perfect square."
+        else:
+            return f"The cube of the hundreds digit ({hundreds}) of number {user_input} is not a perfect square."
+    else:
+        return f"The hundreds digit ({hundreds}) of number {user_input} is not even."
+    
+
+#print(check_rational_square())
+
+############################################################################################
+
+"""
+Q-12 : Write a function that, given a positive rational number with a two-digit decimal part, determines whether the numeric value of the decimal part is a perfect square.
+"""
+
+def check_digit_square():
+    user_input = input("Enter a positive rational number with two decimal places: ")
+    num = float(user_input)
+    
+    # Extract decimal part as integer (two digits)
+    decimal_part = int(round((num - int(num)) * 100))
+    root = int(decimal_part ** 0.5)
+    if decimal_part == root * root:
+        return f"positive rational number with a two-digit decimal part {user_input} is square number" 
+    else:
+        return f"positive rational number with a two-digit decimal part {user_input} is not square number" 
+    
+
+#print(check_digit_square())
+
+
+############################################################################################
+
+"""
+Q_13 : Write a function that, given a positive rational number with a two-digit decimal part, finds the distance of the integer value of the decimal part to the nearest perfect square number.
+"""
+
+def check_nearest_square():
+    user_input = input("Enter a positive rational number with two decimal places: ")
+    num = float(user_input)
+    
+    # Extract decimal part as integer (two digits)
+    decimal_part = int(round((num - int(num)) * 100))
+    
+    square_list = [i * i for i in range(0, 11)]
+    
+    diff_list = [np.abs(decimal_part- sq) for sq in square_list]
+
+    min_diff = min(diff_list)
+    nearest_square = square_list[diff_list.index(min_diff)
+                                 ]
+    return  f" nearest point is {nearest_square} and diff is({min_diff})"
+
+
+#print(check_nearest_square())
 
 ############################################################################################
 
